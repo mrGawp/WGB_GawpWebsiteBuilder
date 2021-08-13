f882448a35e0de232d89cf1ab181216e75842068
mrGawp <88121671+mrGawp@users.noreply.github.com>
2021-08-12 23:12:37 -0400

    Add files via upload
    basic template builder

---
+++ scripts.js
@@ -0,0 +1,215 @@
+function oswald() {
+    var x = document.getElementById("title");
+    x.classList.add("t-oswald");
+    x.classList.remove("t-stylescript");
+    x.classList.remove("t-palettemosaic");
+    x.classList.remove("t-poppins");
+    x.classList.remove("t-arial");
+    x.classList.remove("t-roboto");
+    x.classList.remove("t-montecarlo");
+  }
+function stylescript() {
+    var x = document.getElementById("title");
+    x.classList.add("t-stylescript");
+    x.classList.remove("t-palettemosaic");
+      x.classList.remove("t-oswald");
+      x.classList.remove("t-poppins");
+      x.classList.remove("t-arial");
+      x.classList.remove("t-roboto");
+      x.classList.remove("t-montecarlo");
+  }
+  function palettemosaic() {
+      var x = document.getElementById("title");
+      x.classList.add("t-palettemosaic");
+      x.classList.remove("t-stylescript");
+      x.classList.remove("t-oswald");
+      x.classList.remove("t-poppins");
+      x.classList.remove("t-arial");
+      x.classList.remove("t-roboto");
+      x.classList.remove("t-montecarlo");
+    }
+    function poppins() {
+        var x = document.getElementById("title");
+        x.classList.add("t-poppins");
+        x.classList.remove("t-palettemosaic");
+        x.classList.remove("t-stylescript");
+        x.classList.remove("t-oswald");
+        x.classList.remove("t-arial");
+        x.classList.remove("t-roboto");
+        x.classList.remove("t-montecarlo");
+      }
+      function arial() {
+          var x = document.getElementById("title");
+          x.classList.add("t-arial");
+          x.classList.remove("t-poppins");
+          x.classList.remove("t-palettemosaic");
+          x.classList.remove("t-stylescript");
+          x.classList.remove("t-oswald");
+          x.classList.remove("t-roboto");
+          x.classList.remove("t-montecarlo");
+        }
+          function roboto() {
+              var x = document.getElementById("title");
+              x.classList.add("t-roboto");
+              x.classList.remove("t-arial");
+              x.classList.remove("t-poppins");
+              x.classList.remove("t-palettemosaic");
+              x.classList.remove("t-stylescript");
+              x.classList.remove("t-oswald");
+              x.classList.remove("t-montecarlo");
+            }
+            function montecarlo() {
+                var x = document.getElementById("title");
+                x.classList.add("t-montecarlo");
+                x.classList.remove("t-roboto");
+                x.classList.remove("t-arial");
+                x.classList.remove("t-poppins");
+                x.classList.remove("t-palettemosaic");
+                x.classList.remove("t-stylescript");
+                x.classList.remove("t-oswald");
+              }
+
+// section title fonts ---------------------------------------------------------
+function soswald() {
+    var x = document.getElementById("stitle");
+    x.classList.add("t-oswald");
+    x.classList.remove("t-stylescript");
+    x.classList.remove("t-palettemosaic");
+    x.classList.remove("t-poppins");
+    x.classList.remove("t-arial");
+    x.classList.remove("t-roboto");
+    x.classList.remove("t-montecarlo");
+  }
+function sstylescript() {
+    var x = document.getElementById("stitle");
+    x.classList.add("t-stylescript");
+    x.classList.remove("t-palettemosaic");
+      x.classList.remove("t-oswald");
+      x.classList.remove("t-poppins");
+      x.classList.remove("t-arial");
+      x.classList.remove("t-roboto");
+      x.classList.remove("t-montecarlo");
+  }
+  function spalettemosaic() {
+      var x = document.getElementById("stitle");
+      x.classList.add("t-palettemosaic");
+      x.classList.remove("t-stylescript");
+      x.classList.remove("t-oswald");
+      x.classList.remove("t-poppins");
+      x.classList.remove("t-arial");
+      x.classList.remove("t-roboto");
+      x.classList.remove("t-montecarlo");
+    }
+    function spoppins() {
+        var x = document.getElementById("stitle");
+        x.classList.add("t-poppins");
+        x.classList.remove("t-palettemosaic");
+        x.classList.remove("t-stylescript");
+        x.classList.remove("t-oswald");
+        x.classList.remove("t-arial");
+        x.classList.remove("t-roboto");
+        x.classList.remove("t-montecarlo");
+      }
+      function sarial() {
+          var x = document.getElementById("stitle");
+          x.classList.add("t-arial");
+          x.classList.remove("t-poppins");
+          x.classList.remove("t-palettemosaic");
+          x.classList.remove("t-stylescript");
+          x.classList.remove("t-oswald");
+          x.classList.remove("t-roboto");
+          x.classList.remove("t-montecarlo");
+        }
+          function sroboto() {
+              var x = document.getElementById("stitle");
+              x.classList.add("t-roboto");
+              x.classList.remove("t-arial");
+              x.classList.remove("t-poppins");
+              x.classList.remove("t-palettemosaic");
+              x.classList.remove("t-stylescript");
+              x.classList.remove("t-oswald");
+              x.classList.remove("t-montecarlo");
+            }
+            function smontecarlo() {
+                var x = document.getElementById("stitle");
+                x.classList.add("t-montecarlo");
+                x.classList.remove("t-roboto");
+                x.classList.remove("t-arial");
+                x.classList.remove("t-poppins");
+                x.classList.remove("t-palettemosaic");
+                x.classList.remove("t-stylescript");
+                x.classList.remove("t-oswald");
+              }
+
+
+// section paragraph fonts -----------------------------------------------------
+             function poswald() {
+                  var x = document.getElementById("p");
+                  x.classList.add("t-oswald");
+                  x.classList.remove("t-stylescript");
+                  x.classList.remove("t-palettemosaic");
+                  x.classList.remove("t-poppins");
+                  x.classList.remove("t-arial");
+                  x.classList.remove("t-roboto");
+                  x.classList.remove("t-montecarlo");
+                }
+              function pstylescript() {
+                  var x = document.getElementById("p");
+                  x.classList.add("t-stylescript");
+                  x.classList.remove("t-palettemosaic");
+                    x.classList.remove("t-oswald");
+                    x.classList.remove("t-poppins");
+                    x.classList.remove("t-arial");
+                    x.classList.remove("t-roboto");
+                    x.classList.remove("t-montecarlo");
+                }
+                function ppalettemosaic() {
+                    var x = document.getElementById("p");
+                    x.classList.add("t-palettemosaic");
+                    x.classList.remove("t-stylescript");
+                    x.classList.remove("t-oswald");
+                    x.classList.remove("t-poppins");
+                    x.classList.remove("t-arial");
+                    x.classList.remove("t-roboto");
+                    x.classList.remove("t-montecarlo");
+                  }
+                  function ppoppins() {
+                      var x = document.getElementById("p");
+                      x.classList.add("t-poppins");
+                      x.classList.remove("t-palettemosaic");
+                      x.classList.remove("t-stylescript");
+                      x.classList.remove("t-oswald");
+                      x.classList.remove("t-arial");
+                      x.classList.remove("t-roboto");
+                      x.classList.remove("t-montecarlo");
+                    }
+                    function parial() {
+                        var x = document.getElementById("p");
+                        x.classList.add("t-arial");
+                        x.classList.remove("t-poppins");
+                        x.classList.remove("t-palettemosaic");
+                        x.classList.remove("t-stylescript");
+                        x.classList.remove("t-oswald");
+                        x.classList.remove("t-roboto");
+                        x.classList.remove("t-montecarlo");
+                      }
+                        function proboto() {
+                            var x = document.getElementById("p");
+                            x.classList.add("t-roboto");
+                            x.classList.remove("t-arial");
+                            x.classList.remove("t-poppins");
+                            x.classList.remove("t-palettemosaic");
+                            x.classList.remove("t-stylescript");
+                            x.classList.remove("t-oswald");
+                            x.classList.remove("t-montecarlo");
+                          }
+                          function pmontecarlo() {
+                              var x = document.getElementById("p");
+                              x.classList.add("t-montecarlo");
+                              x.classList.remove("t-roboto");
+                              x.classList.remove("t-arial");
+                              x.classList.remove("t-poppins");
+                              x.classList.remove("t-palettemosaic");
+                              x.classList.remove("t-stylescript");
+                              x.classList.remove("t-oswald");
+                            }
